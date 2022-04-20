4351bf93c3c3388964c30cf51855c939684fc1e8
sedo31149@gmail.com <sedo31149@gmail.com>
2022-04-21 06:46:52 +0700

    upload

---
+++ config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  showStats: true, // Booleen | true / e
+	website: {
+		protocol: 'https://', // https:// or http://
+		domain: 'bot-Website-Template-EJS-1.putraaaa.repl.co', // website link without protocol ex. bot-website.tejas1794.repl.co
+		port: 3000,
+    cookieSecret: 'bot-website-template-ejs-1.putraaaa.repl.co/' // Cookie secret | charaters used store client data
+	},
+  bot: {
+    name: 'Felysia', // Bot Name
+    token: 'OTY2MjUwOTAwODAyOTA0MDk0.Yl_BRg.jbJHvpKNo52wHLNqAnH1Bigg1Uc', // Bot token if showing Stats
+    logo: '', //Bot Logo used in embeds and favicon
+    description: 'test', //description of your bot used in embed and website
+    support: 'tedt', // support server url of your bot
+    invite: 'tesr', // invite link of your bot
+    botlist: 'test', // Voting website Ex. top.gg
+    feature1: {
+      main: 'gvc', // Feature 1 Heading
+      description: 'cccc' // Feature 1 Description
+    },
+    feature2: {
+      main: 'vvgf', // Feature 2 Heading
+      description: 'gggh' // Feature 1 Description
+    },
+    feature3: {
+      main: 'hhh', // Feature 3 Heading
+      description: 'ggg' // Feature 1 Description
+    }
+  },
+  img: {
+    img1: 'https://i.redd.it/2yd002ox8ku81.jpg', // help cmd image of your bot ex. https://cdn.discordapp.com/attachments/868823136538333184/889852973713354823/Capture.PNG
+    img2: 'https://i.redd.it/2yd002ox8ku81.jpg' // most famous command image of your bot Ex. https://cdn.discordapp.com/attachments/868823136538333184/889853204697849926/Capture.PNG
+  }
+  
+};
